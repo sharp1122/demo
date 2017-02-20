dd4defbeff7892ea1c705fc513f3f0e6fcb308b3
xieping <312488100@qq.com>
2017-02-20 15:26:57 +0800

    去掉注释

--- src/main/java/com/sharp/demo/test/StringFormat.java
+++ src/main/java/com/sharp/demo/test/StringFormat.java
@@ -8,24 +8,24 @@ public class StringFormat {
 
 	public static void main(String[] args) {
 		
-//		String str = "你好！%s我要去%s，结果可能不%s";
-//		
-//		str = String.format(str, "明天","广州","理想");
-//		
-//		System.out.println(str);
-//		
-//		
-//		str = "你好！结果可能不{1}，{3}我要去{0}";
-//		
-//		str = MessageFormat.format(str, "广州","理想","明天");
-//		
-//		System.out.println(str);
-//		
-//		str = "http://120.24.94.126:80/health/web/appInvite/joinToGroup.html";
-//		
-//		str = str.replaceFirst("health/web", "health/web/bd_h5");
-//		
-//		System.out.println(str);
+		String str = "你好！%s我要去%s，结果可能不%s";
+		
+		str = String.format(str, "明天","广州","理想");
+		
+		System.out.println(str);
+		
+		
+		str = "你好！结果可能不{1}，{3}我要去{0}";
+		
+		str = MessageFormat.format(str, "广州","理想","明天");
+		
+		System.out.println(str);
+		
+		str = "http://120.24.94.126:80/health/web/appInvite/joinToGroup.html";
+		
+		str = str.replaceFirst("health/web", "health/web/bd_h5");
+		
+		System.out.println(str);
 		
 		long timestamp = 1474387200000L;
 		
